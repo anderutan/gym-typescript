f7466b1506a041dc4736788550e90f27e70792af
anderu <andrewtan1310@gmail.com>
2024-05-09 08:42:37 +0800

    Add benefits part

--- src/share/types.ts
+++ src/share/types.ts
@@ -1,6 +1,12 @@
 export enum SelectedPage {
   Home = "home",
-  Benefits = "benefit",
+  Benefits = "benefits",
   OurClasses = "ourclasses",
   ContactUs = "contactus",
+}
+
+export type BenefitType = {
+  icon: JSX.Element,
+  title: string,
+  description: string,
 }
